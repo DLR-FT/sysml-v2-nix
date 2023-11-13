795205306961217741beb68304b1f965a641492d
wucke13 <wucke13@gmail.com>
2023-11-13 18:43:37 +0100

    further refinement of flake, build still fails

--- flake.nix
+++ flake.nix
@@ -5,36 +5,44 @@
   inputs.sbt.url = "github:zaninime/sbt-derivation";
   inputs.sbt.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils, sbt }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (f: p: { jre = p.jdk17_headless; })
-          ];
-        };
-      in
-      {
-        packages = rec {
-          sysml-v2-api-server = sbt.mkSbtDerivation.${system} rec {
-            sbt = pkgs.sbt.override { jre = pkgs.jdk17_headless; };
-            pname = "SysML-v2-API-Services";
-            version = "2023-02";
-            src = pkgs.fetchFromGitHub {
-              owner = "Systems-Modeling";
-              repo = pname;
-              rev = version;
-              sha256 = "sha256-kel3zWaIUE7AtiXQMuQ4nYJ9ln892XlukDOp5MOLg3c=";
-            };
-            depsSha256 = "";
-            nativeBuildInputs = with pkgs; [
-              openjdk17 # LTS with Sec
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (f: p: { jre = p.jdk17_headless; })
             ];
           };
-        };
 
-        devShells.default = pkgs.mkShell { };
-      }
-    );
+          sbt = pkgs.sbt.override { jre = pkgs.jdk17_headless; };
+
+          # necessary because of
+          # https://github.com/zaninime/sbt-derivation/issues/17
+          mkSbtDerivation = inputs.sbt.mkSbtDerivation.${system}.withOverrides
+            {
+              inherit sbt;
+            };
+        in
+        {
+          packages = rec {
+            sysml-v2-api-server = mkSbtDerivation rec {
+              pname = "SysML-v2-API-Services";
+              version = "2023-02";
+              src = pkgs.fetchFromGitHub {
+                owner = "Systems-Modeling";
+                repo = pname;
+                rev = version;
+                sha256 = "sha256-kel3zWaIUE7AtiXQMuQ4nYJ9ln892XlukDOp5MOLg3c=";
+              };
+              depsSha256 = "";
+            };
+          };
+
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [ sbt ];
+          };
+        }
+      );
 }
