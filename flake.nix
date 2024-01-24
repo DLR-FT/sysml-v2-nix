6fccbb3a391ebfc83861b95ff571170a701daf05
Wanja Zaeske <wanja.zaeske@dlr.de>
2024-01-24 16:21:26 +0100

    chore: factor out api-services pkg

--- flake.nix
+++ flake.nix
@@ -6,84 +6,19 @@
   inputs.sbt.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        packages = rec {
+          default = sysml-v2-api-server;
+
+          sysml-v2-api-server = pkgs.callPackage pkgs/sysml-v2-api-services.nix {
+            mkSbtDerivation = inputs.sbt.mkSbtDerivation.${system};
           };
-
-          # long live the legacy...
-          # 2023-11-13: jdk17 breaks, the upstream projects requires Java 11
-          java = pkgs.jdk11_headless;
-          sbt = pkgs.sbt.override { jre = java; };
-
-          # necessary because of
-          # https://github.com/zaninime/sbt-derivation/issues/17
-          mkSbtDerivation = inputs.sbt.mkSbtDerivation.${system}.withOverrides
-            {
-              inherit sbt;
-            };
-        in
-        {
-          packages = rec {
-            default = sysml-v2-api-server;
-            sysml-v2-api-server = mkSbtDerivation rec {
-              pname = "SysML-v2-API-Services";
-              version = "2023-02";
-              src = pkgs.fetchFromGitHub {
-                owner = "Systems-Modeling";
-                repo = pname;
-                rev = version;
-                sha256 = "sha256-kel3zWaIUE7AtiXQMuQ4nYJ9ln892XlukDOp5MOLg3c=";
-              };
-              depsSha256 = "sha256-UjMEHHLalfxQOv1w0hZgFHE7KgkOJOCSCDhZpvb4ffg=";
-
-              patches = [
-                ./emf-use-system-properties.patch
-                ./env-var-play-application-secret.patch
-
-                # see https://jdbc.postgresql.org/documentation/use/#unix-sockets for details
-                ./enable-unix-domain-socket-postgres.patch
-              ];
-
-              # remove hardcoded, un-overridable Database credentials
-              # more info:
-              # https://stackoverflow.com/a/17594064
-              postPatch = ''
-                sed '/javax\.persistence\.jdbc/d' --in-place conf/META-INF/persistence.xml
-              '';
-
-              buildPhase = ''
-                runHook preBuild
-                sbt dist
-                runHook postBuild
-              '';
-
-              installPhase = ''
-                runHook preInstall
-                unzip target/universal/*.zip
-                mv sysml-* $out
-                runHook postInstall
-              '';
-              preFixup = ''
-                wrapProgram $out/bin/sysml-v2-api-services \
-                  --suffix PATH : ${pkgs.lib.makeBinPath (with pkgs; [ gawk java ])}
-              '';
-              nativeBuildInputs = with pkgs; [
-                makeWrapper
-                unzip
-              ];
-              meta.mainProgram = "sysml-v2-api-services";
-            };
-          };
-
-          devShells.default = pkgs.mkShell {
-            nativeBuildInputs = [
-              java
-              pkgs.postgresql
-              sbt
-            ];
-          };
-        });
+        };
+      });
 }
---
+++ pkgs/sysml-v2-api-services.nix
@@ -0,0 +1,68 @@
+{ lib, mkSbtDerivation, system, sbt, fetchFromGitHub, makeWrapper, unzip, gawk, jdk11_headless }:
+
+let
+  pname = "SysML-v2-API-Services";
+  mainProgram = lib.strings.toLower pname;
+
+  # long live the legacy...
+  # 2023-11-13: jdk17 breaks, the upstream projects requires Java 11
+  java = jdk11_headless;
+
+  sbtWithCustomJava = sbt.override { jre = java; };
+
+  # necessary because of
+  # https://github.com/zaninime/sbt-derivation/issues/17
+  mkSbtDerivationWithCustomJava = mkSbtDerivation.withOverrides {
+    sbt = sbtWithCustomJava;
+  };
+
+in
+mkSbtDerivationWithCustomJava rec {
+  pname = "SysML-v2-API-Services";
+  version = "2023-02";
+  src = fetchFromGitHub {
+    owner = "Systems-Modeling";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-kel3zWaIUE7AtiXQMuQ4nYJ9ln892XlukDOp5MOLg3c=";
+  };
+  depsSha256 = "sha256-UjMEHHLalfxQOv1w0hZgFHE7KgkOJOCSCDhZpvb4ffg=";
+
+  patches = [
+    ../patches/emf-use-system-properties.patch
+    ../patches/env-var-play-application-secret.patch
+
+    # see https://jdbc.postgresql.org/documentation/use/#unix-sockets for details
+    ../patches/enable-unix-domain-socket-postgres.patch
+  ];
+
+  nativeBuildInputs = [
+    makeWrapper
+    unzip
+  ];
+
+  # remove hardcoded, un-overridable Database credentials
+  # more info:
+  # https://stackoverflow.com/a/17594064
+  postPatch = ''
+    sed '/javax\.persistence\.jdbc/d' --in-place conf/META-INF/persistence.xml
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+    sbt dist
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    unzip target/universal/*.zip
+    mv sysml-* $out
+    runHook postInstall
+  '';
+  preFixup = ''
+    wrapProgram $out/bin/sysml-v2-api-services \
+      --suffix PATH : ${lib.makeBinPath ([ gawk java ])}
+  '';
+  meta = { inherit mainProgram; };
+}
