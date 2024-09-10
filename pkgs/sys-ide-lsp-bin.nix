7b6ea1755f5df33842122cfd60222f756ca5c97b
wucke13 <wucke13@gmail.com>
2024-09-10 14:46:58 +0200

    chore: sys-ide-{lsp,vscode}-bin 0.6.0 -> 0.6.2

--- pkgs/sys-ide-lsp-bin.nix
+++ pkgs/sys-ide-lsp-bin.nix
@@ -3,11 +3,11 @@
 stdenvNoCC.mkDerivation (finalAttrs: {
 
   pname = "syside";
-  version = "0.6.0";
+  version = "0.6.2";
   src = fetchurl {
     # TODO find stable link that involves the version number
-    url = "https://gitlab.com/sensmetry/public/sysml-2ls/-/jobs/6403788212/artifacts/raw/syside-languageserver.js";
-    hash = "sha256-wPVPTFFl07Qu1lxVtwLmzGZ+XmMacKsuu1oIAPxqKPU=";
+    url = "https://gitlab.com/sensmetry/public/sysml-2ls/-/jobs/7234621313/artifacts/raw/syside-languageserver.js";
+    hash = "sha256-0YPfFrmB3MWZ9/MvxzpfZFUA7OtVv5thTE/BDqNXTJQ=";
   };
 
   dontUnpack = true;
